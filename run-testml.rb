5d0d98a6751454b8c936bb2d4d1a536e1a2ae88c
Ingy döt Net <ingy@ingy.net>
2013-05-01 00:48:07 -0700

    clean up runner a bit

--- run-testml.rb
+++ run-testml.rb
@@ -2,9 +2,8 @@
 
 require 'test/unit'
 require 'testml'
-require 'testml/util'
 require 'testml/bridge'
-require 'tempfile'
+require 'testml/util'
 
 class TestMLBridge < TestML::Bridge
   include TestML::Util
@@ -27,14 +26,10 @@ class Test::TestML < Test::Unit::TestCase
   files.each do |file|
     method_name = 'test_' + file.gsub(/\W/, '_').sub(/_tml$/, '')
     define_method(method_name.to_sym) do
-      run_testml_file(file)
+      TestML.new(
+        testml: File.read(file),
+        bridge: TestMLBridge,
+      ).run(self)
     end
-  end
-
-  def run_testml_file(file)
-    TestML.new(
-      testml: File.read(file),
-      bridge: TestMLBridge,
-    ).run(self)
   end
 end
