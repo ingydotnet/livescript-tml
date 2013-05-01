0a833fb8cc958a7df31cd383c4856b18ff686fb9
Ingy döt Net <ingy@ingy.net>
2013-05-01 00:45:32 -0700

    first commit

---
+++ run-testml.rb
@@ -0,0 +1,40 @@
+#!/usr/bin/env ruby
+
+require 'test/unit'
+require 'testml'
+require 'testml/util'
+require 'testml/bridge'
+require 'tempfile'
+
+class TestMLBridge < TestML::Bridge
+  include TestML::Util
+  def compile(livescript)
+    javascript = ''
+    IO.popen('livescript --compile --bare', 'r+') { |p|
+      p.puts(livescript.value)
+      p.close_write
+      while not p.eof?
+        javascript += p.read
+      end
+    }
+    return str javascript
+  end
+end
+
+class Test::TestML < Test::Unit::TestCase
+  files = ARGV.empty? ? Dir.glob('./*.tml') : ARGV.clone
+
+  files.each do |file|
+    method_name = 'test_' + file.gsub(/\W/, '_').sub(/_tml$/, '')
+    define_method(method_name.to_sym) do
+      run_testml_file(file)
+    end
+  end
+
+  def run_testml_file(file)
+    TestML.new(
+      testml: File.read(file),
+      bridge: TestMLBridge,
+    ).run(self)
+  end
+end
